12807ada2ca0ceb1a81ebd75c69df733ab64635b
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-02 22:39:49 +0300

    Commit for changes

--- src/app.mjs
+++ src/app.mjs
@@ -1,8 +1,7 @@
 import express from 'express';
 
+
 const app = express();
-
-
 
 
 
--- src/server.mjs
+++ src/server.mjs
@@ -1,3 +1,18 @@
 import app from "./app.mjs";
+import http from 'http';
+import 'dotenv/config'
 
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+
+
+
+try {
+    server.listen(PORT,()=>{
+        console.log(`Server is listening on port ${PORT}`);
+    })
+}catch (error) {
+    console.log("Error occurred: ", error);
+}
 
