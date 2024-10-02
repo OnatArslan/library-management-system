45e5bec3a061d7057e3bfff268b938a49061ada2
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-02 22:43:48 +0300

    Commit for changes(test)

--- src/app.mjs
+++ src/app.mjs
@@ -1,9 +1,9 @@
 import express from 'express';
-
+import testRouter from "./routers/testRouter.mjs";
 
 const app = express();
 
 
-
+app.use(`/test`, testRouter)
 export default app;
 
--- src/routers/testRouter.mjs
+++ src/routers/testRouter.mjs
@@ -1,5 +1,8 @@
 import express from "express";
-
+import {getTest} from "../controllers/testController.mjs";
 const router = express.Router();
 
-router.route(`/`).get()
+router.route(`/`).get(getTest)
+
+
+export default router
