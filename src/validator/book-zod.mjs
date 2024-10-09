d691dec5d9d61d157cb21cc748f968343e3a5a70
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-09 14:07:12 +0300

    Commit for changes

---
+++ src/validator/book-zod.mjs
@@ -0,0 +1,20 @@
+import {z} from "zod";
+import isISBN from "validator/es/lib/isISBN.js";
+
+
+const bookZod = {
+    id: z.string().cuid().optional(),
+    isbn: z.string().refine((val) => isISBN(val), {
+        message: `Invalid ISBN format`
+    }),
+    title: z.string().min(2, `Book title must have at least 2 characters`),
+    authors: z.array(z.string()),
+    category: z.string(),
+    isBooked: z.boolean().default(false),
+    publishedAt: z.date().optional(),
+    
+    
+    
+};
+
+export default bookZod;
