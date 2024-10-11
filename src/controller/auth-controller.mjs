c225c2f3f5c2df35ca14f87f4c6094bdf9eed7c5
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-11 11:22:11 +0300

    Commit for changes

--- src/controller/auth-controller.mjs
+++ src/controller/auth-controller.mjs
@@ -6,11 +6,14 @@ import jwt from "jsonwebtoken"
 export const signUp = async(req,res,next) =>{
     try {
         let validData
-        let rawData = req.body;
+        let {email, password, confirmPassword,username} = req.body;
         /* Check data is valid */
         try{
             validData = userZod.parse({
-              ...rawData,
+              email:email,
+              username: username,
+              password: password,
+              confirmPassword: confirmPassword,
               role:"USER"
             });
         }catch(e){
@@ -115,15 +118,33 @@ export const authenticate = async(req,res,next) =>{
   try {
 // 1. Extract the token from the request cookies.
     const token = req.cookies.token
-    console.log(token)
-    /*
-    2. Verify the JWT token.
-    3. Decode the token to get the user ID.
-    4. Fetch the user from the database using the user ID.
-    5. If the user does not exist, return an error.
-    6. Attach the user to the request object.
-    7. Call the `next` middleware function.
-    */
+    if(!token) return next(new Error(`Token is missing!!`))
+    
+    // 2. Verify the JWT token.
+    let decoded
+    try{
+      decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    }catch(e) {
+      return next(new Error(`Token is invalid or has expired`));
+    }
+    
+    // 3. Find user with given decoded id
+    const user = await prisma.user.findUnique({
+      where:{
+        id: decoded.id,
+        
+      }
+    })
+    if(!user){
+      return next(new Error(`A user belongs to this token has deleted...`))
+    }
+    
+    // Check if password changed after token is created +10000 for when register times will be the same
+    if (user.passwordChangedAt.getTime() > decoded.iat * 1000 + 10000) {
+      return next(new Error(`Password changed after token creation. Please login again!`));
+    }
+    // If all is done, call next middleware for accessing routes
+    req.user = user;
     next();
   }catch(e) {
     next(e)
@@ -133,13 +154,13 @@ export const authenticate = async(req,res,next) =>{
 
 export const getAllUsers = async(req,res,next) =>{
     try {
-        const users = await prisma.user.findMany();
-        
-        res.status(200).json({
-            status:`success`,
-            data:{
-                users
-            }
+      const users = await prisma.user.findMany();
+      console.log(req.user);
+      res.status(200).json({
+        status:`success`,
+        data:{
+          users
+        }
         })
     }catch(e) {
         next(e)
