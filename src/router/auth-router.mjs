f5d10e422c9a851076dae991e0f79fd237a1eb91
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-18 23:21:48 +0300

    Commit for changes

--- src/router/auth-router.mjs
+++ src/router/auth-router.mjs
@@ -20,7 +20,7 @@ router.route(`/log-out`).delete(logOut)
 router.route(`/forgot-password`).post(forgotPassword)
 router.route(`/reset-password/:resetString`).patch(resetPassword)
 
-// Remove from here
+
 router.route(`/get-me`).get(authenticate,getMe)
 
 export default router;
