23b9c32109d16eb8ada646570e8c4e663aab5633
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-19 21:33:52 +0300

    Commit for changes

--- src/router/auth-router.mjs
+++ src/router/auth-router.mjs
@@ -13,13 +13,13 @@ import {
 
 const router = express.Router();
 
-
 router.route(`/sign-up`).post(signUp)
 router.route(`/sign-in`).post(signIn)
 router.route(`/log-out`).delete(logOut)
 router.route(`/forgot-password`).post(forgotPassword)
 router.route(`/reset-password/:resetString`).patch(resetPassword)
 
+// sss
 
 router.route(`/get-me`).get(authenticate,getMe)
 
