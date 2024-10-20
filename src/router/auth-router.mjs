a725739003cd49f83e94bed1222974270f1f6cf2
OnatArslan <onatarslan@posta.mu.edu.tr>
2024-10-20 23:36:05 +0300

    Commit for changes

--- src/router/auth-router.mjs
+++ src/router/auth-router.mjs
@@ -19,7 +19,7 @@ router.route(`/log-out`).delete(logOut)
 router.route(`/forgot-password`).post(forgotPassword)
 router.route(`/reset-password/:resetString`).patch(resetPassword)
 
-
+// Commit here
 
 router.route(`/get-me`).get(authenticate,getMe)
 
